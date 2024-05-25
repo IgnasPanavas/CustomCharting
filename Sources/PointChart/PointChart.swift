f6e6c5b8f38a08bae1bd02ab5191d60a24ae1d14
Ignas <ignaspanava@gmail.com>
2024-05-25 01:24:12 -0400

    .

--- Sources/PointChart/PointChart.swift
+++ Sources/PointChart/PointChart.swift
@@ -1,8 +1,35 @@
-//
-//  File.swift
-//  
-//
-//  Created by Ignas Panavas on 5/25/24.
-//
+import SwiftUI
 
-import Foundation
+public struct PointChart: View {
+    let dataPoints: [(x: CGFloat, y: CGFloat)] // Array of (x, y) data
+    
+    public init(dataPoints: [(x: CGFloat, y: CGFloat)]) {
+        self.dataPoints = dataPoints
+    }
+    
+    @available(iOS 13.0.0, *)
+    public var body: some View {
+        GeometryReader { geometry in
+            ZStack {
+                Path { path in // Draw the connecting lines
+                    path.move(to: CGPoint(x: 0, y: geometry.size.height))
+                    for point in dataPoints {
+                        let x = point.x * geometry.size.width
+                        let y = geometry.size.height - (point.y * geometry.size.height)
+                        path.addLine(to: CGPoint(x: x, y: y))
+                    }
+                }
+                .stroke(Color.blue, style: StrokeStyle(lineWidth: 2, lineCap: .round))
+                
+                ForEach(dataPoints, id: \.x) { point in // Plot the points
+                    Circle()
+                        .fill(Color.red)
+                        .frame(width: 8, height: 8)
+                        .position(x: point.x * geometry.size.width,
+                                  y: geometry.size.height - (point.y * geometry.size.height))
+                }
+            }
+            .padding()
+        }
+    }
+}
